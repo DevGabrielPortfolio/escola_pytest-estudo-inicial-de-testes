b170f6ab47539ca84275e962b9225fcaae76d158
Gabriel Belentani Felipe <gabriel.portifolio07@gmail.com>
2025-08-05 11:28:36 -0300

    ADD: new verification of data

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,5 +1,11 @@
 def calcular_media(numeros:list[float]) -> float:
 
+    #   Verificando se é uma lista
+    if not isinstance(numeros, list[float]):
+
+        raise ValueError("the data provided is not a list")
+
+    #   Verificando se a lista esta vazia
     if not numeros:
 
         raise ValueError("it is not allowed to send an empty list")
