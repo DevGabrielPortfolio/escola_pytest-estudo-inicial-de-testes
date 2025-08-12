9f7c27a986b237a0c4c5e4fa7b3953237a38496c
Gabriel Belentani Felipe <gabriel.portifolio07@gmail.com>
2025-08-12 10:51:35 -0300

    MODIFY: Adicionando mais testes

--- escola/aluno.py
+++ escola/aluno.py
@@ -1,10 +1,30 @@
 def calcular_media(numeros:list[float]) -> float:
+    """
+        Calcula a média de uma lista de notas.
+
+        Parâmetros:
+        notas (list[float]): Lista de notas a serem calculadas
+
+        Retorna:
+        float: A média das notas, arredondada para uma casa decimal
+    """
 
     #   Verificando se a lista esta vazia
     if not numeros:
 
         raise ValueError("it is not allowed to send an empty list")
     
+    for nota in numeros:
+        # Verifica se a nota é uma string
+        if isinstance(nota, str):
+            
+            raise TypeError("invalid note: texts are not allowed")
+
+        # Verifica se a nota é maior que 10 ou menor que 0
+        if(nota < 0 or nota > 10):
+            raise ValueError("grades can be from 0 to 10")
+
+
     soma = sum(numeros)
     quantidade = len(numeros)
     media = soma / quantidade
--- tests/test_calcular_media_excecoes.py
+++ tests/test_calcular_media_excecoes.py
@@ -10,3 +10,30 @@ def test_calcular_media_lita_vazia():
     #   Executando a função e esperando erro
     with pytest.raises(ValueError, match="it is not allowed to send an empty list"):
         calcular_media(entrada)
+
+
+def test_calcular_media_com_string_ao_invez_de_lista_numerica():
+    entrada2 = "ola"
+
+    with pytest.raises(TypeError, match="invalid note: texts are not allowed"):
+        calcular_media(entrada2)
+
+
+def test_calcular_media_com_string_e_numero_na_lista():
+    entrada3 = ["ola", 3.0]
+
+    with pytest.raises(TypeError, match="invalid note: texts are not allowed"):
+        calcular_media(entrada3)
+
+
+def test_calcular_media_com_numero_negativo_na_lista():
+    entrada4 = [-10.0]
+
+    with pytest.raises(ValueError, match="grades can be from 0 to 10"):
+        calcular_media(entrada4)
+
+def test_calcular_media_com_numero_maior_que_10_na_lista():
+    entrada5 = [-10.0]
+
+    with pytest.raises(ValueError, match="grades can be from 0 to 10"):
+        calcular_media(entrada5)
